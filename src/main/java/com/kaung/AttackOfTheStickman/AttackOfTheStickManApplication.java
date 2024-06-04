f44a569802296bc6e36e4ea256808d8efa3f26ef
Lycanx4 <kgmyatlwin89@gmail.com>
2024-06-03 22:31:01 -0400

    Add Project to remote

---
+++ src/main/java/com/kaung/AttackOfTheStickman/AttackOfTheStickManApplication.java
@@ -0,0 +1,14 @@
+package com.kaung.AttackOfTheStickman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttackOfTheStickManApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AttackOfTheStickManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaung/AttackOfTheStickman/AttackOfTheStickManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaung.AttackOfTheStickman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttackOfTheStickManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
